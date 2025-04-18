f2dfb7ee7b395cb31b42cee08577240b376baa69
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-18 16:04:34 +0300

    implents writing a test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,7 +1,7 @@
 import { currentAgeForBirthYear } from "../utils";
 
-describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
+describe("currentAgeForBirthYear", () => { // describe
+  it("returns the age of a person based on the year of birth", () => { //it && test
     const birthYear = 1984;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
     expect(ageOfPerson).toBe(38);
